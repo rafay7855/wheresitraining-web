c7636c95e604f9832a3f9cedac4648755fb07339
unknown <rafay7855@gmail.com>
2020-10-17 19:18:46 -0400

    A little app and template

--- wheresitraining/app.py
+++ wheresitraining/app.py
@@ -1,1 +1,9 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def hello():
+    return render_template('frontpage.html')
